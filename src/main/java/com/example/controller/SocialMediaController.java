c02d637965bcc787dbda639f007d9dd421b669e2
EonWizard Lee <brmilee02@gmail.com>
2025-04-09 01:43:24 +0000

    Add Spring Boot Functionalities

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,10 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +12,22 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    
+    // @PostMapping("/register")
 
+    // @PostMapping("/login")
+
+    // @PostMapping("/messages")
+
+    // @GetMapping("/messages")
+
+    // @GetMapping("/messages/{message_id}")
+
+    // @DeleteMapping("/messages/{message_id}")
+
+    // @PatchMapping("/messages/{message_id}")
+
+    // @GetMapping("/accounts/{account_id}/messages")
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,25 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    // create new message
+
+    // get all messages
+    
+    // get message by id
+
+    // delete message by id
+
+    // update message by id
+
+    // get user messages based on account id
 }
